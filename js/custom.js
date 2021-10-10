33511fb1aa0b1fd35383127a935e965e5d0517db
Stefan Zhelyazkov <stefan.zhelyazkov@gmail.com>
2021-10-10 21:11:55 +0300

    Clean up styles (#1)
    * First changes
    
    * Remove logo
    
    * fix resolution for larger dppx
    
    * order styles for max-width 479px
    
    * align items better in about section
    
    * align bio information in profile for small screens
    
    * Update jQuery to 3.6 and update plugins
    
    * Complete styles for max-width < 480px

--- js/custom.js
+++ js/custom.js
@@ -1,204 +1,203 @@
-jQuery(document).ready(function(){ 
-	
+jQuery(document).ready(function () {
+
 	/* ---------------------------------------------------------------------- */
 	/*	Custom Functions
 	/* ---------------------------------------------------------------------- */
 
 	// Logo
-	var $logo 	= $('#logo');
+	var $logo = $('#logo');
 
-    if (location.href.indexOf("#") != -1) {
-        $logo.show();
-    }
+	if (location.href.indexOf("#") != -1) {
+		$logo.show();
+	}
 	// Show logo 
-	$('.menu .tabs a').click(function() {
-	  $logo.fadeIn('slow');
+	$('.menu .tabs a').click(function () {
+		$logo.fadeIn('slow');
 	});
 	// Hide logo
-	$('.tab-profile').click(function() {
-	  $logo.fadeOut('slow');
-	});	
+	$('.tab-profile').click(function () {
+		$logo.fadeOut('slow');
+	});
 
 	/* ---------------------------------------------------------------------- */
 	/*	Resume
 	/* ---------------------------------------------------------------------- */
-	
+
 	// Rating bars
-	$(".skills li .rating").each(function(index,e) { 
+	$(".skills li .rating").each(function (index, e) {
 
 		// Vars
-		var 
+		var
 			$ratNum = 7,
 			$rat = $(e).attr("data-rat"),
 			$point = "<span></span>";
 
 		// Append points
-		while($ratNum > 0){
-		     $(e).append($point);
-		     $ratNum--;
+		while ($ratNum > 0) {
+			$(e).append($point);
+			$ratNum--;
 		}
 
-		$(e).find("span").each(function(index,e) { 
-			if(index >= $rat) return false;
+		$(e).find("span").each(function (index, e) {
+			if (index >= $rat) return false;
 			// Append Disabled Rats
 			$(e).animate({
-			    opacity: 1
-			  });
+				opacity: 1
+			});
 		});
 
-    });
-	 
+	});
+
 	/* ---------------------------------------------------------------------- */
 	/*	Menu
 	/* ---------------------------------------------------------------------- */
-	
+
 	// Needed variables
-	var $content 		= $("#content");
-	
+	var $content = $("#content");
+
 	// Run easytabs
-  	$content.easytabs({
-	  animate			: true,
-	  updateHash		: false,
-	  transitionIn		:'slideDown',
-	  transitionOut		:'slideUp',
-	  animationSpeed	:600,
-	  tabs				:".tmenu",
-	  tabActiveClass	:'active',
+	$content.easytabs({
+		animate: true,
+		updateHash: false,
+		transitionIn: 'slideDown',
+		transitionOut: 'slideUp',
+		animationSpeed: 600,
+		tabs: ".tmenu",
+		tabActiveClass: 'active',
 	});
 
-	
+
 	// Hover menu effect
 	$content.find('.tabs li a').hover(
-		function() {
+		function () {
 			$(this).stop().animate({ marginTop: "-7px" }, 200);
-		},function(){
+		}, function () {
 			$(this).stop().animate({ marginTop: "0px" }, 300);
 		}
 	);
 
 	// Menu Navigation
-	 $(".menu .tabs").carouFredSel({
-        responsive          : true,
-        direction           : "left",
- 	    circular: false,
-    	infinite: false,
-        pagination  		: "#menu-controls",  
-        auto    			: false,
-        scroll 			: {
-            items           : 1,
-            duration        : 300,                        
-            wipe    : true
-        },
-		prev	: {	
-			button	: "#menu-prev",
-			key		: "right"
+	$(".menu .tabs").carouFredSel({
+		responsive: true,
+		direction: "left",
+		circular: false,
+		infinite: false,
+		pagination: "#menu-controls",
+		auto: false,
+		scroll: {
+			items: 1,
+			duration: 300,
+			wipe: true
 		},
-		next	: { 
-			button	: "#menu-next",
-			key		: "left"
+		prev: {
+			button: "#menu-prev",
+			key: "right"
 		},
-	    swipe: {
-	        onTouch: true
-	    },
-        items: {
-            width: 140,
-            visible: {
-              min: 2,
-              max: 5
-            }
-        }           
-    });
+		next: {
+			button: "#menu-next",
+			key: "left"
+		},
+		swipe: {
+			onTouch: true
+		},
+		items: {
+			width: 140,
+			visible: {
+				min: 2,
+				max: 5
+			}
+		}
+	});
 	/* ---------------------------------------------------------------------- */
 	/*	Cats Filter
-	/* ---------------------------------------------------------------------- */ 
-	
-	var $catsfilter 		= $('.cats-filter');
+	/* ---------------------------------------------------------------------- */
+
+	var $catsfilter = $('.cats-filter');
 
 	// Copy categories to item classes
-	$catsfilter.find('a').click(function() {
+	$catsfilter.find('a').click(function () {
 		var currentOption = $(this).attr('data-filter');
 		$(this).parent().parent().find('a').removeClass('current');
 		$(this).addClass('current');
-	});	
+	});
 
 	/* ---------------------------------------------------------------------- */
 	/*	Portfolio
-	/* ---------------------------------------------------------------------- */ 
-	
+	/* ---------------------------------------------------------------------- */
+
 	// Needed variables
-	var $plist	 	= $('#projects-list');
-	var $pfilter 		= $('#projects-filter');
-		
+	var $plist = $('#projects-list');
+	var $pfilter = $('#projects-filter');
+
 	// Run Isotope  
 	$plist.isotope({
-		filter				: '*',
-		layoutMode   		: 'masonry',
-		animationOptions	: {
-		duration			: 750,
-		easing				: 'linear'
-	   }
-	});	
-	
+		filter: '*',
+		layoutMode: 'masonry',
+		animationOptions: {
+			duration: 750,
+			easing: 'linear'
+		}
+	});
+
 	// Isotope Filter 
-	$pfilter.find('a').click(function(){
-	  var selector = $(this).attr('data-filter');
-		$plist.isotope({ 
-		filter				: selector,
-		animationOptions	: {
-		duration			: 750,
-		easing				: 'linear',
-		queue				: false,
-	   }
-	  });
-	  return false;
-	});	
-	
+	$pfilter.find('a').click(function () {
+		var selector = $(this).attr('data-filter');
+		$plist.isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 750,
+				easing: 'linear',
+				queue: false,
+			}
+		});
+		return false;
+	});
+
 	// Portfolio image animation 
 	$plist.find('img').adipoli({
-		'startEffect' 	: 'transparent',
-		'hoverEffect' 	: 'boxRandom',
-		'imageOpacity' 	: 0.6,
-		'animSpeed' 	: 100,
+		'startEffect': 'transparent',
+		'hoverEffect': 'boxRandom',
+		'imageOpacity': 0.6,
+		'animSpeed': 100,
 	});
-	
+
 	/* ---------------------------------------------------------------------- */
 	/*	prettyPhoto
 	/* ---------------------------------------------------------------------- */
 
-    $("a[rel^='projects']").prettyPhoto({
-    	animation_speed: 'fast', /* fast/slow/normal */
-    	social_tools: '',
-    	theme: 'pp_default',
-    	horizontal_padding: 5,
-    });
+	$("a[rel^='projects']").prettyPhoto({
+		animation_speed: 'fast', /* fast/slow/normal */
+		social_tools: '',
+		theme: 'pp_default',
+		horizontal_padding: 5,
+	});
 
 	/* ---------------------------------------------------------------------- */
 	/*	Contact Form
 	/* ---------------------------------------------------------------------- */
-	
+
 	// Needed variables
-	var $contactform 	= $('#contactform'),
-		$success		= 'Your message has been sent. Thank you!';
-		
-	$contactform.submit(function(){
+	var $contactform = $('#contactform'),
+		$success = 'Your message has been sent. Thank you!';
+
+	$contactform.submit(function () {
 		$.ajax({
-		   type: "POST",
-		   url: "php/contact.php",
-		   data: $(this).serialize(),
-		   success: function(msg)
-		   {
-				if(msg == 'SEND'){
-					response = '<div class="success">'+ $success +'</div>';
+			type: "POST",
+			url: "php/contact.php",
+			data: $(this).serialize(),
+			success: function (msg) {
+				if (msg == 'SEND') {
+					response = '<div class="success">' + $success + '</div>';
 				}
-				else{
-					response = '<div class="error">'+ msg +'</div>';
+				else {
+					response = '<div class="error">' + msg + '</div>';
 				}
 				// Hide any previous response text
 				$(".error,.success").remove();
 				// Show response message
 				$contactform.prepend(response);
 			}
-		 });
+		});
 		return false;
-	});	
-});	
+	});
+});
